d1e0eedc77e4917ac5fa5409c4a16ac4315d0e54
khasanov-ibroxim <xibroxim11@gmail.com>
2026-07-04 19:51:23 +0500

    asd

--- app/[lang]/contact/page.tsx
+++ app/[lang]/contact/page.tsx
@@ -15,7 +15,7 @@ const Page = () => {
             <div className="">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d187.27343022292354!2d69.21377251390744!3d41.32246083785528!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1783160525636!5m2!1sru!2s"
-                    width="100%" height="550"  allowFullScreen="" loading="lazy"
+                    width="100%" height="550"   loading="lazy"
                     referrerPolicy="strict-origin-when-cross-origin"></iframe>
             </div>
             <Contact_s3/>
