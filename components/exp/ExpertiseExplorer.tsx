77107dc73b727f4530e2f34bf693355215e70e65
khasanov-ibroxim <xibroxim11@gmail.com>
2026-07-08 20:06:51 +0500

    asd

--- components/exp/ExpertiseExplorer.tsx
+++ components/exp/ExpertiseExplorer.tsx
@@ -133,7 +133,7 @@ export default function ExpertiseExplorer({
                 {/* Kartochkalar grid */}
                 <div>
                     {filteredPosts.length > 0 ? (
-                        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3">
+                        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2">
                             {filteredPosts.map((post) => (
                                 <ExpertPostCard key={post.id} post={post} lang={lang} />
                             ))}
